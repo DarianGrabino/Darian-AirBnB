3fa7b91088dbae3162259c83871bf216d6913cba
DarianGrabino <dariangra@gmail.com>
2023-07-05 14:56:44 -0700

    add storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """BaseModel class"""
 import uuid
 from datetime import datetime
+import models
 
 format = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -18,6 +19,8 @@ class BaseModel:
             self.id =str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            from models import storage
+            storage.new(self)
 
     def __str__(self):
         """define la representación en forma de cadena de un objeto"""
@@ -26,6 +29,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
         self.update_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__ of the instance """
