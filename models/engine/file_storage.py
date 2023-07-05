ade74b22ace4e5d2df99d466ae86a5a726756200
DarianGrabino <dariangra@gmail.com>
2023-07-05 14:52:48 -0700

    serializes and deserializes

---
+++ models/engine/file_storage.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""that serializes instances to a JSON file and deserializes JSON file"""
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """class FileStorage"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """ add an object in the dictionary "__objects" using 
+        the class name and its attribute id """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = {}
+        for key, value in self.__objects.items():
+            obj_dict[key] = value.to_dict()
+        with open(self.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r", encoding="utf-8") as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    obj = BaseModel(**value)
+                    self.__objects[key] = obj
